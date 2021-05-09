12c97538844b55267e6faddc78289961a721318e
viconstel <k.elizarov@rambler.ru>
2021-05-09 21:09:21 +0300

    Add test for custom standard scaler

---
+++ homework1/tests/test_custom_scaler.py
@@ -0,0 +1,36 @@
+import sys
+import os
+ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.insert(0, os.path.join(ROOT_DIR, 'ml_project'))
+import pytest
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+from features import CustomStandardScaler
+
+
+TRAIN_DATA_PATH = r'data\heart.csv'
+TEST_DATA_PATH = r'data\test_sample.csv'
+
+
+def test_custom_standard_scaler():
+    train_df = pd.read_csv(os.path.join(ROOT_DIR, TRAIN_DATA_PATH))
+    test_df = pd.read_csv(os.path.join(ROOT_DIR, TEST_DATA_PATH))
+
+    sklearn_scaler = StandardScaler()
+    custom_scaler = CustomStandardScaler()
+
+    sklearn_train_data = sklearn_scaler.fit_transform(train_df)
+    custom_train_data = custom_scaler.fit_transform(train_df)
+
+    assert np.allclose(sklearn_train_data, custom_train_data.to_numpy()), (
+        'Different results with sklearn and custom scaler on train data.'
+    )
+
+    sklearn_test_data = sklearn_scaler.fit_transform(test_df)
+    custom_test_data = custom_scaler.fit_transform(test_df)
+
+    assert np.allclose(sklearn_test_data, custom_test_data.to_numpy()), (
+        'Different results with sklearn and custom scaler on test data.'
+    )

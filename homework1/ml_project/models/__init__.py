57af8a5cf444e5b9b306ac5bfda20fe797393d00
viconstel <k.elizarov@rambler.ru>
2021-05-09 19:15:16 +0300

    Model interaction methods

---
+++ homework1/ml_project/models/__init__.py
@@ -0,0 +1,17 @@
+from .model import (
+    train_model,
+    predict_model,
+    evaluate_model,
+    save_model,
+    save_metrics,
+    load_model,
+)
+
+__all__ = [
+    "train_model",
+    "predict_model",
+    "evaluate_model",
+    "save_model",
+    "save_metrics",
+    "load_model",
+]
